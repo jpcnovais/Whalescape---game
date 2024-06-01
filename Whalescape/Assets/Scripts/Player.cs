6d856ac092def940570ed1be817d2c8b9c03ceda
Inês Martins <inesmart1203@gmail.com>
2024-06-01 23:21:11 +0100

    movimento baleia

--- Whalescape/Assets/Scripts/Player.cs
+++ Whalescape/Assets/Scripts/Player.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    [SerializeField]
+    private float movementSpeed;
+    private float horizontal;
+
+    [SerializeField]
+    private float jumpPower;
+    private bool grounded;
+    private bool canDoubleJump;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontal = -Input.GetAxis("Horizontal"); // Invertendo o valor
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+                canDoubleJump = true;
+                grounded = false;
+            }
+            else if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                playerRb.velocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+                playerRb.AddForce(Vector3.up * (jumpPower / 1.75f), ForceMode.Impulse);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        playerRb.velocity = new Vector3(playerRb.velocity.x, playerRb.velocity.y, horizontal * movementSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            grounded = false;
+        }
     }
 }
