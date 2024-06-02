a7c78ccce54c675bb7b3ac345e87306a1c71a911
JDiogo5 <josediogo_06@hotmail.com>
2024-06-02 17:31:38 +0100

    aumento de tamanho

--- Whalescape/Assets/Scripts/MainCamera.cs
+++ Whalescape/Assets/Scripts/MainCamera.cs
@@ -23,7 +23,7 @@ public class CamaraPrincipal : MonoBehaviour
         }
 
 
-        offset = new Vector3(-0.8f, 0.2f, 0f);
+        offset = new Vector3(-2f, 0.2f, 0f);
     }
 
     void LateUpdate()
