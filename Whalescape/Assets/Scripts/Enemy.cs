615de44d4a1579f2917ff0b4cd2ff342ce914379
Inês Martins <inesmart1203@gmail.com>
2024-06-02 18:39:41 +0100

    inimigo T-T

--- Whalescape/Assets/Scripts/Enemy.cs
+++ Whalescape/Assets/Scripts/Enemy.cs
@@ -16,11 +16,9 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private bool isMovingLeft;
 
-    private float tolerance = 0.1f; // Tolerância para evitar tremores
-
     void Start()
     {
-        if (checkpoint1.position.x > checkpoint2.position.x)
+        if (checkpoint1.position.z > checkpoint2.position.z)
         {
             var aux = checkpoint2;
             checkpoint2 = checkpoint1;
@@ -30,32 +28,30 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        // Depuração para verificar a posição do inimigo
-        Debug.Log("Update - Posição do inimigo: " + transform.position);
+
     }
 
     void FixedUpdate()
     {
         if (isMovingLeft)
         {
-            if (transform.position.x <= checkpoint1.position.x + tolerance)
+            if (transform.position.z < checkpoint1.position.z)
             {
                 isMovingLeft = !isMovingLeft;
-                Debug.Log("Chegou ao checkpoint1, mudando direção para a direita");
             }
         }
         else
         {
-            if (transform.position.x >= checkpoint2.position.x - tolerance)
+            if (transform.position.z > checkpoint2.position.z)
             {
                 isMovingLeft = !isMovingLeft;
-                Debug.Log("Chegou ao checkpoint2, mudando direção para a esquerda");
             }
         }
+
         int direction = isMovingLeft ? -1 : 1;
         transform.position += new Vector3(direction * speed * Time.deltaTime, 0, 0);
 
-        // Log para verificar direção e posição
-        Debug.Log("FixedUpdate - Posição do inimigo: " + transform.position + ", direção: " + (isMovingLeft ? "esquerda" : "direita"));
+        // Virar o inimigo na direção do movimento
+        transform.rotation = Quaternion.Euler(0, isMovingLeft ? 180 : 0, 0);
     }
 }
