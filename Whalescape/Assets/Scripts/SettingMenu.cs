612b0fb08485244a225e68b7469c27c8aefc3917
Joao Novais <150026595+jpcnovais@users.noreply.github.com>
2024-06-03 11:32:50 +0100

    menu_pausa

--- Whalescape/Assets/Scripts/SettingMenu.cs
+++ Whalescape/Assets/Scripts/SettingMenu.cs
@@ -6,6 +6,9 @@ using UnityEngine.Audio;
 
 public class SettingMenu : MonoBehaviour
 {
+
+    [SerializeField] GameObject pausaMenu;
+    [SerializeField] GameObject settingsMenu;
     public Slider masterVolume, musicVolume;
     public AudioMixer mainAudioMixer;
 
@@ -23,6 +26,12 @@ public class SettingMenu : MonoBehaviour
     public void SetFullScreen (bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
+    }
+
+    public void CloseSettings()
+    {
+        pausaMenu.SetActive(true);
+        settingsMenu.SetActive(false);
     }
 
 }
